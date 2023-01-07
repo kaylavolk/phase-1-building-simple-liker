20b9c259ef40ce3a60f47d138e8c36fd4273d0ac
Kayla Volk <kaylavolk8@gmail.com>
2023-01-06 21:59:11 -0500

    commit tree c7ae65481862ebada4fd8c592d4e5639a94faa52

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const errorModal = document.querySelector("#modal");
+errorModal.classList.add("hidden");
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM CONTENT HAS LOADED");
+  
+
+  clickListener();
+});
+
+
+function hideError() {
+  errorModal.classList.add("hidden");
+}
+
+  function findLikes() {
+    const likeArr = document.querySelectorAll(".like-glyph")
+
+    likeArr.forEach((singularLike) => {
+      singularLike.addEventListener("click", () =>
+      console.log("YOU FOUND ME! LIKE!"))
+    })
+  }
+
+  function clickListener(){
+    document.addEventListener("click", (event) => {
+
+      if (event.target.classList[0] === 'like-glyph') {
+      
+      mimicServerCall()
+      .then(resp => {
+       const activated = event.target.classList.contains("activated-heart");
+       if (activated){
+       event.target.classList.remove("activated-heart")
+       event.target.innerHTML = EMPTY_HEART
+       } else{
+        event.target.classList.add("activated-heart")
+        event.target.innerHTML = FULL_HEART
+       }
+      
+      })
+      .catch(error => {
+        setTimeout(() => {
+          hideError()
+        }, 3000)
+        }) 
+      }
+    })
+  }
 
 
 
